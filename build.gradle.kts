dc451e1404bb25c2980f482d7b6ff1f5d7d252b1
UnTamed-Fury <prince.16.tamoli@gmail.com>
2025-05-14 09:22:16 +0530

    Alpha release test 14

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-// Top-level build.gradle.kts
+// Root build.gradle.kts
 
 buildscript {
     repositories {
@@ -7,21 +7,14 @@ buildscript {
         maven("https://jitpack.io")
     }
     dependencies {
-        // Android Gradle Plugin
         classpath("com.android.tools.build:gradle:8.1.0")
-        // Shortcut Gradle plugin
-        classpath("com.github.Android-Shortcut:shortcut-gradle-plugin:1.1.0") // Replace with actual version
     }
 }
 
 plugins {
-    // Kotlin Serialization
     id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22" apply false
-    // AboutLibraries
     id("com.mikepenz.aboutlibraries.plugin") version "10.8.3" apply false
-    // MOKO resources
     id("dev.icerock.mobile.multiplatform-resources") version "0.23.0" apply false
-    // SQLDelight
     id("com.squareup.sqldelight") version "1.5.5" apply false
 }
 
