291c7fe22da3bf79d3c09ecd996fe6558f5feaad
UnTamed-Fury <prince.16.tamoli@gmail.com>
2025-05-14 07:07:29 +0530

    updated build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,35 @@
+// Top‐level build.gradle.kts
+
 buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
     dependencies {
+        // Android Gradle Plugin from version catalog
+        classpath(libs.android.gradlePlugin)
+        // Shortcut Gradle plugin
         classpath(libs.android.shortcut.gradle)
     }
 }
 
 plugins {
-    alias(kotlinx.plugins.serialization) apply false
-    alias(libs.plugins.aboutLibraries) apply false
-    alias(libs.plugins.moko) apply false
-    alias(libs.plugins.sqldelight) apply false
+    // Register Kotlin Serialization, AboutLibraries, MOKO, SQLDelight, etc.
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.aboutLibraries)       apply false
+    alias(libs.plugins.moko)                 apply false
+    alias(libs.plugins.sqldelight)           apply false
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
 }
 
 tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
+    delete(rootProject.buildDir)
 }
